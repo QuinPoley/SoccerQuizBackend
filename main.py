9f42e884bcb0c2527cde80c5bcfb7b2f270e42d3
QuinPoley <quin.poley@gmail.com>
2021-07-14 17:05:40 -0400

    Added fetch all

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+#uvicorn main:app --reload
 from fastapi import FastAPI
 
 app = FastAPI()
+Items = ["Item1", "Item2", "Item3"]
+
 
 @app.get("/")
 def home():
     return {"Hello": "FastAPI"}
+
+
+@app.get("/items")
+def getAllQuizes():
+    returnvalue = "{"
+    for x in range(len(Items)):
+        returnvalue = returnvalue + Items[x]
+    returnvalue = returnvalue + "}"
+    return returnvalue
